833687be8783394a9c8d5417551f3ca1318bd758
rajdeepietlko27 <rajdeepsingh141204@gmail.com>
2026-05-02 01:33:07 +0530

    fixed

--- Middleware/admin-middleware.js
+++ Middleware/admin-middleware.js
@@ -2,7 +2,7 @@ const isAdminUser = (req, res, next)=>{
     if(req.userInfo.role !== 'admin'){
         return res.status(403).json({
             success : false,
-            message: "Access denied! Admin rights required"
+            message: "Access failed! Admin rights required"
         })
     }
 
